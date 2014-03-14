cdef721aff652ab790844ffd13b97b45c6965849
Niklas Hökenström <niklashokenstrom@gmail.com>
2014-03-14 13:12:36 +0100

    Sekventiell start fungerar

--- erlang_projekt/add.erl
+++ erlang_projekt/add.erl
@@ -2,6 +2,9 @@
 -module(add).
 -export([start/3, start/4]).
 
+%% To use EUnit we must include this.
+-include_lib("eunit/include/eunit.hrl").
+
 %% @doc TODO: add documentation
 -spec start(A,B,Base) -> ok when 
       A::integer(),
@@ -10,7 +13,8 @@
 
 start(A,B, Base) when Base >= 2 andalso Base =< 36 ->
     {Al,Bl} = special:fill_list(utils:integerToListBase(A,Base),utils:integerToListBase(B,Base)),
-    utils:add(Al,Bl,Base).
+    {Result, Carry} = utils:add(Al,Bl,Base),
+    special:print_result(Carry, Al, Bl, Result).
     
 
 %% @doc TODO: add documentation
@@ -26,29 +30,20 @@ start(A,B,Base, Options) ->
 
 
 
-
-
-
-%add_([A1|A],[B1|B], Base, Acc, Carry) when Base =< 10 ->
-%    if
-%	A1 + B1 > Base -> 
-%	    NewCarry = 1, 
-%	    Result = A1 + B1 + Carry - Base;
-%	true -> 
-%	    NewCarry = 0,
-%	    Result = A1 + B1 + Carry
-%    end.
-%
-%
-%add_(A,B, Base, Result, NewCarry).
-%
-%add_([A1|A],[B1|B], Base, Acc, Carry) when Base =< 32 ->
-%    
-%
-
-
-
-
-
-%add(A,B,Base, Acc, 0) ->
-%    add_(lists:reverse(A), lists:reverse(B), Base, Acc).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%                                                                          %%
+%%			   EUnit Test Cases                                 %%
+%%                                                                          %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+		     
+    
+result_test_() ->
+    Assert = fun(A,B,Base) ->
+		     {Al,Bl} =  special:fill_list(utils:integerToListBase(A,Base),utils:integerToListBase(B,Base)),
+		     {Result,_Carry} = utils:add(Al,Bl,Base),
+		     ?_assertEqual(A+B,list_to_integer(special:int_list_to_string(Result),Base))
+	     end, 
+    random:seed(erlang:now()),
+    [Assert(A,B,Base) || A <- [random:uniform(1000000) || _ <- lists:seq(1,8)],
+			      B <- [random:uniform(1000000) || _ <- lists:seq(1,8)],
+			      Base <- lists:seq(2,36)].
--- erlang_projekt/special.erl
+++ erlang_projekt/special.erl
@@ -8,6 +8,38 @@
 -compile(export_all). 
 
 
+line(N) ->
+    utils:repeat($-, N).
+
+remove_all_zeros(List) ->
+    F = fun(X) when X =:= 48 -> X-16; (X) -> X  end,
+    lists:map(F,List).
+
+create_carry_line(List) ->
+    F = fun(X) when X =:= 1 -> 45; (_X) -> 32  end,
+    lists:map(F,List).
+
+remove_first_zeros([]) ->
+    [];
+remove_first_zeros([48|T]) ->
+    [32|remove_first_zeros(T)];
+remove_first_zeros(List) ->
+    List.
+    
+int_list_to_string(List) ->
+    F = fun(X) when X < 10 -> X+48; (X) -> X+55 end,
+    lists:map(F,List).
+		
+
+print_result(Carry, A, B, Result) ->
+    io:format("  " ++ remove_all_zeros(int_list_to_string(Carry))++"~n"),
+    io:format("  " ++ create_carry_line(Carry)++"~n"),
+    io:format("   " ++ remove_first_zeros(int_list_to_string(A))++"~n"),
+    io:format("   " ++ remove_first_zeros(int_list_to_string(B))++"~n"),
+    io:format("+ " ++ line(length(Result))++"~n"),
+    io:format("  " ++ remove_first_zeros(int_list_to_string(Result))++"~n")
+    .
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% @doc Adds Difference zeros to List
 add_zero(List, 0) ->
